a2dedab473e3e849199f76e8e3f80465e0f31dfd
AKryshnia <151573670+AKryshnia@users.noreply.github.com>
2024-05-20 16:14:09 +0300

    Add files via upload

--- FSTR/FSTR/settings.py
+++ FSTR/FSTR/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'FSTR.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'FSTRdatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'ApT-HEk-Rhy-Eh7',
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
 
