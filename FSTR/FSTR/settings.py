f017f8c40ce49a192c3b77ce5a42ebfc7ce2a0bc
AKryshnia <151573670+AKryshnia@users.noreply.github.com>
2024-05-20 16:37:54 +0300

    Add files via upload

--- FSTR/FSTR/settings.py
+++ FSTR/FSTR/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DBpereval',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'ApT-HEk-Rhy-Eh7',
         'HOST': 'localhost',
-        'POST': '5432',
+        'PORT': '5432',
     }
 }
 
