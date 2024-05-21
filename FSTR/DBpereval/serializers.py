d89f4134fab1ceef1324021a8f8355246abda3a9
AKryshnia <151573670+AKryshnia@users.noreply.github.com>
2024-05-21 15:20:14 +0300

    Update serializers.py
    Refactored a "create" function for images in the PerevalAddedSerializer

--- FSTR/DBpereval/serializers.py
+++ FSTR/DBpereval/serializers.py
@@ -28,15 +28,40 @@ class LevelSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class PerevalImageSerializer(serializers.HyperlinkedModelSerializer):
-   class Meta:
+    image = serializers.URLField()
+
+    class Meta:
        model = PerevalImage
-       fields = ['data', 'title', 'pereval', ]
+       fields = ['image', 'title', 'pereval', ]
 
 
 class PerevalAddedSerializer(serializers.Serializer):
-    pereval = PerevalSerializer()
     user = TouristSerializer()
     coords = CoordinatesSerializer()
     level = LevelSerializer()
-    image = PerevalImageSerializer()
+    images = PerevalImageSerializer(many=True)
+
+    class Meta:
+        model = Pereval
+        fields = ['beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'user', 'coords', 'winter', 'spring',
+                  'summer', 'autumn', 'status', 'images']
+
+    def create(self, validated_data, **kwargs):
+        user = validated_data.pop('user')
+        coords = validated_data.pop('coords')
+        level = validated_data.pop('level')
+        images = validated_data.pop('image')
+
+        user, created = Tourist.objects.get_or_create(**user)
+
+        coords = Coordinates.objects.create(**coords)
+        level = Level.objects.create(**level)
+        pereval = Pereval.objects.create(**validated_data, user=user, coords=coords, level=level, status='NW')
+
+        for i in images:
+            image = i.pop('image')
+            title = i.pop('title')
+            PerevalImage.objects.create(image=image, pereval=pereval, title=title)
+
+        return pereval
 
